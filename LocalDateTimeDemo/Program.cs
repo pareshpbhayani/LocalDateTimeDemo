a04ef48578abf995cdf862fe07e08516df8ec6e8
pareshpbhayani <pareshpbhayani@gmail.com>
2024-12-17 19:57:34 +0530

    Add project files.

---
+++ LocalDateTimeDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime localTime = DateTime.Now;
+Console.WriteLine($"Local DateTime: {localTime}");
+
+DateTime utcTime = localTime.ToUniversalTime();
+Console.WriteLine($"UTC DateTime: {utcTime}");
+
+DateTimeOffset localTimeOffset = DateTimeOffset.Now;
+Console.WriteLine($"Local DateTime with Offset: {localTimeOffset}");
